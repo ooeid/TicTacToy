9a2f72be4a4f32d02a907d5c1c54cb03aa29d88e
ooeid <ooeid.omar@gmail.com>
2019-12-02 16:18:29 +0300

    rdy to lunch!

--- main.js
+++ main.js
@@ -1,91 +1,114 @@
 // calling our var 
-let x = "X"
+
 let btn = $('.btn button');
 let count = 0
+let player1 = 0
+let countP1 = $('aside .p1') // to count player1 wins
+let player2 = 0
+let countP2 = $('aside .p2') // to count player2 wins
 
 
-//nextTrun 
+// Our alerting funcations 
 
-function turn(event){
+let winAlert = function () {
+    alert('YOU ARE THE WINNER!')
+}
+let loseAlert = function () {
+    alert('YOOU ARE THE LOOSER!')
+}
+let drawAlert = function () {
+    alert('DRAW!')
+}
 
-    count += 1
-    $(event.target).text(x)
-    $(event.target).off();
-    
-    if (x ==='X') {
-        x = 'O'
+
+// check winner funcation for player X
+
+function checkWinnerX() {
+    if ($('.btn1').text() === 'X' && $('.btn2').text() === 'X' && $('.btn3').text() === 'X') { // 1
+        setTimeout(winAlert, 250)
+         countP1.text(player1 +=1) 
+    } else if ($('.btn4 ').text() === 'X' && $('.btn5').text() === 'X' && $('.btn6').text() === 'X') { // 2
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
+    } else if ($('.btn7').text() === 'X' && $('.btn8').text() === 'X' && $('.btn9').text() === 'X') { // 3
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
+    } else if ($('.btn2').text() === 'X' && $('.btn5').text() === 'X' && $('.btn8').text() === 'X') { //4 
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
+    } else if ($('.btn1').text() === 'X' && $('.btn4').text() === 'X' && $('.btn7').text() === 'X') { //5 
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
+    } else if ($('.btn3').text() === 'X' && $('.btn6').text() === 'X' && $('.btn9').text() === 'X') {  //6 
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
+    } else if ($('.btn1').text() === 'X' && $('.btn5').text() === 'X' && $('.btn9').text() === 'X') { //7 
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
+    } else if ($('.btn3').text() === 'X' && $('.btn5').text() === 'X' && $('.btn7').text() === 'X') { // 8 
+        setTimeout(winAlert, 250)
+        countP1.text(player1 +=1)
     }
-    else if (x ==='O') {
-        x = 'X'
-        
+}
+
+// check winner funcation for player O
+
+function checkWinnerO() {
+    if ($('.btn1').text() === 'O' && $('.btn2').text() === 'O' && $('.btn3').text() === 'O') { // 1
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn4 ').text() === 'O' && $('.btn5').text() === 'O' && $('.btn6').text() === 'O') { // 2
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn7').text() === 'O' && $('.btn8').text() === 'O' && $('.btn9').text() === 'O') { // 3
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn2').text() === 'O' && $('.btn5').text() === 'O' && $('.btn8').text() === 'O') { //4 
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn1').text() === 'O' && $('.btn4').text() === 'O' && $('.btn7').text() === 'O') { //5 
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn3').text() === 'O' && $('.btn6').text() === 'O' && $('.btn9').text() === 'O') { //6 
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn1').text() === 'O' && $('.btn5').text() === 'O' && $('.btn9').text() === 'O') { //7 
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    } else if ($('.btn3').text() === 'O' && $('.btn5').text() === 'O' && $('.btn7').text() === 'O') { // 8 
+        setTimeout(loseAlert, 250)
+        countP2.text(player2 +=1)
+    }
+
+}
+//our playing funcation 
+
+function turn(event) {
+
+    if (count % 2 == 0) {
+        $(event.target).text('X');
+        $(event.target).off();
+    } else { 
+        $(event.target).text('O');
+        $(event.target).off();
+
     }
     checkWinnerX()
     checkWinnerO()
-    checkDraw()
-    
-}
-
-// check winner funcation for player X
-function checkWinnerX() {
-if ( $('.btn1').text()==='X' && $('.btn2').text()==='X' && $('.btn3').text()==='X'){  // 1
-    alert('YOU ARE THE WINNER'); 
-    
-}
-else if($('.btn4 ').text()==='X' && $('.btn5').text()==='X' && $('.btn6').text()==='X') { // 2
-    alert('YOU ARE THE WINNER')
-}   else if ($('.btn7').text()==='X' && $('.btn8').text()==='X' && $('.btn9').text()==='X' ){ // 3
-    alert('YOU ARE THE WINNER')
-}       else if ($('.btn2').text()==='X' && $('.btn5').text()==='X' && $('.btn8').text()==='X'){ //4 
-    alert('YOU ARE THE WINNER')
-}           else  if ($('.btn1').text()==='X' && $('.btn4').text()==='X' && $('.btn7').text()==='X') { //5 
-    alert('YOU ARE THE WINNER')
-}               else if ($('.btn3').text()==='X' && $('.btn6').text()==='X' && $('.btn9').text()==='X'){ //6 
-    alert('YOU ARE THE WINNER')
-}                   else if ($('.btn1').text()==='X' && $('.btn5').text()==='X' && $('.btn9').text()==='X'){ //7 
-    alert('YOU ARE THE WINNER')
-     
-}  else if ($('.btn3').text()==='X' && $('.btn5').text()==='X' && $('.btn7').text()==='X'){ // 8 
-    alert('YOU ARE THE WINNER')
-} else {
-    
-}
+    count += 1
 
 }
+// resting our game 
 
-// check winner funcation for player X
+function restGame() { 
 
-function checkWinnerO() {
-    if ( $('.btn1').text()==='O' && $('.btn2').text()==='O' && $('.btn3').text()==='O'){  // 1
-        alert('YOU ARE THE LOOSER')
-    $() }
-    else if($('.btn4 ').text()==='O' && $('.btn5').text()==='O' && $('.btn6').text()==='O') { // 2
-        alert('YOU ARE THE LOOSER')
-    }   else if ($('.btn7').text()==='O' && $('.btn8').text()==='O' && $('.btn9').text()==='O' ){ // 3
-        alert('YOU ARE THE LOOSER')
-    }       else if ($('.btn2').text()==='O' && $('.btn5').text()==='O' && $('.btn8').text()==='O'){ //4 
-        alert('YOU ARE THE LOOSER')
-    }           else  if ($('.btn1').text()==='O' && $('.btn4').text()==='O' && $('.btn7').text()==='O') { //5 
-        alert('YOU ARE THE LOOSER')
-    }               else if ($('.btn3').text()==='O' && $('.btn6').text()==='O' && $('.btn9').text()==='O'){ //6 
-        alert('YOU ARE THE LOOSER')
-    }                   else if ($('.btn1').text()==='O' && $('.btn5').text()==='O' && $('.btn9').text()==='O'){ //7 
-        alert('YOU ARE THE LOOSER')
-         
-    }  else if ($('.btn3').text()==='O' && $('.btn5').text()==='O' && $('.btn7').text()==='O'){ // 8 
-        alert('YOU ARE THE LOOSER')
-
-    }
-    
-    }
-    
-// DRAW! 
-
-function checkDraw (){
- if (count === 9) {
-     alert('DARW!')
-
- }
-
+    count = 0
+    btn.text('')
+    btn.off();
+    btn.click(turn);
 }
+
+// calling our funcation 
 
 btn.click(turn);
+$('#rest').click(restGame);
